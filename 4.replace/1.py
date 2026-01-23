e195d2592552b92516189b661b6cf766c6da657b
sahibcyber <isgenderovhaci12@gmail.com>
2026-01-23 21:12:15 +0400

    4.replace

---
+++ 4.replace/1.py
@@ -0,0 +1,9 @@
+# 77 -i 55 ile evez edek
+
+
+cumle = '077 270 29 00'
+
+
+print(cumle.replace('77', '55'))
+
+#  count - metodunu biz burda daha yaxsi istifade ede bilerik 
---
+++ 4.replace/2.py
@@ -0,0 +1,16 @@
+# eger bu metodu 3 cu parametri versek onda demis oluruq ki, 
+
+# 1 ci tapdigini
+# 2 ci tapdigini
+# 3 cu tapdigini
+# 4 cu tapdigini
+
+
+
+cumle = '077 270 29 00'
+
+print(cumle.replace('77', '55' , 1) ) 
+print(cumle.replace('77', '55' , 2) ) 
+print(cumle.replace('77', '55' , 3) ) 
+print(cumle.replace('77', '55' , 4) ) 
+
