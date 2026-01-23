bc461d5bde2f3a3550e7097ad70afbfcacc1527d
sahibcyber <isgenderovhaci12@gmail.com>
2026-01-23 21:14:54 +0400

    5.input

---
+++ 5.input/1.py
@@ -0,0 +1,10 @@
+# input - bu metodu sahe yaradir . ve bu sahede istediyimiz her seyi yaza bilirik
+
+
+ad = input("senin adin nedir ? ")
+yash = input("senin yash necedir ? ")
+boy = input("senin boyun necedir ?")
+
+print("salam" + ad )
+print("salam" + str(yash) + "yash var" )
+print("salam boyum" + str(boy) + "- dir" )
