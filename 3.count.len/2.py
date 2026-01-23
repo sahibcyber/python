cc066155dbaf85e06d913b41caab86d78b6b735c
sahibcyber <isgenderovhaci12@gmail.com>
2026-01-23 21:07:49 +0400

    3.count.len

---
+++ 3.count.len/2.py
@@ -0,0 +1,13 @@
+# eger tam deqiq oyrenmek istiyirsinizse o zaman len() metodunan istifade ede bilersiniz
+
+
+cumle = '077 270 29 00'
+
+print(cumle.count('')) 
+
+
+print(len(cumle)) 
+
+
+# len metodu hemise deqiq gosterir 
+# bu cumlede necedene simvol oldugunu oyernmek isteyirsinizse 0 zaman len metodunan istifade etmelsiniz
---
+++ 3.count.len/3.py
@@ -0,0 +1,9 @@
+# len() varsa count() neye lazimdir ?
+
+cumle = '077 270 29 00'
+
+
+print(cumle.count('77'))
+
+
+# bu o demekdirki count metodu cumlelerde nece dene o simvoldan istifade oldugunu bilmek ucundur
