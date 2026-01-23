4abaa989dbb66d3620c2637f2586a899a0c7a4f0
sahibcyber <isgenderovhaci12@gmail.com>
2026-01-23 20:54:15 +0400

    1 multi

---
+++ 1.multi/1.py
@@ -0,0 +1,12 @@
+# multiple Asigment - çoxlu atama , toplu verme.
+# birden cox variable - larin bir duz xet uzre ard - arda yazilaraq onlara ferqli deyer verilmesine toplu atama deyilir 
+
+name = "selena" 
+age = 21
+atractive = True
+
+print(name)
+print(age)
+print(atractive)
+
+# bele yazaraq alt - alta print metodunan istifade ederek terminalda cap etdire bilirik
---
+++ 2.py
@@ -0,0 +1,14 @@
+# multiple Assigment - coxlu atama , toplu verme
+
+
+daniel = 25
+selena = 25 
+sahib = 25 
+ibrahim = 25
+
+
+print(daniel)
+print(selena)
+print(sahib)
+print(ibrahim)
+
