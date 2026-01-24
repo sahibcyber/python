1ddfc5f5f297ce1a5e6f018a10ad0b0531b2d2de
sahibcyber <isgenderovhaci12@gmail.com>
2026-01-24 19:46:40 +0400

    10.identity

---
+++ 10.identity/1.py
@@ -0,0 +1,14 @@
+# indentity operators : 
+
+   # a)   is          - her ikisi variable eynidirse , true qaytarir
+   # b)   is not      - her ikisi variable eyni deyilse , true qayitarir
+
+
+x = ["apple", "banana"]
+y = ["apple", "banana"]
+
+z = x
+
+print( x is z )              # true qayidirl,  cunku z ile x eynidir
+
+print( x is not z )          # false qaydir , cunku z ile x eynidir
