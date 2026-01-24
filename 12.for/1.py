36f64423048c6db83b242747fa690f1febdd383d
sahibcyber <isgenderovhaci12@gmail.com>
2026-01-24 20:22:35 +0400

    12.for

---
+++ 12.for/1.py
@@ -0,0 +1,22 @@
+for i in range(10):
+    print(i)
+
+#bu 10 kimi say demekdi 
+
+
+    for i in range(5, 10):
+        print(i) 
+
+
+#bu 5 den basla 10 da qutar demekdi
+
+
+
+
+    for i in range(5, 10, 2):
+        print(i) 
+
+
+
+
+#bu ise 5 den basla 2 ile atla ve 10 da qutar demekdi
---
+++ 12.for/2.py
@@ -0,0 +1,11 @@
+eded1 = input("bir eded daxil edin")
+
+for i in (5, 100):
+    print(i)
+
+
+
+eded2 = input("bir eded daxil edin")
+
+for i in (5, 10):
+    print(i)
