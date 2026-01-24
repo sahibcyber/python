a937e2b8ca762e61c89d830d83bb31206fc2a6ad
sahibcyber <isgenderovhaci12@gmail.com>
2026-01-24 19:32:42 +0400

    7.pow

---
+++ 7.pow/1.py
@@ -0,0 +1,11 @@
+# pow ne demekdir ? pow o demekdir ki ededdi ustu demekdir 
+
+# 2 * 2 * 2 * 2 * 2 *
+
+import math
+
+eded = 2 
+
+print( pow(eded, 5) )
+
+# bele yazaraqda yazdigimiz ededi , eded ustu ede bilirik ve ap et dediyimizdede hesablayaraq getrir
