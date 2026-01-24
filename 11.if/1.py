96a9c5303db7e3b2eb68ff98954cbbcbfdb6c997
sahibcyber <isgenderovhaci12@gmail.com>
2026-01-24 19:55:12 +0400

    11.if

---
+++ 11.if/1.py
@@ -0,0 +1,10 @@
+# if - eger demekdir ve sorgu ifadelerinde istifade edirik. (if) sual veririk. meselen , eger senin yasin 100 - durse
+
+yash = int(  input('senin nece yasin var ') )
+
+
+if yash == 100:
+    print("senin bir esirdir yasiyirsan besdi artiq")
+
+else:
+    print("hele cavansan yasa")
---
+++ 11.if/2.py
@@ -0,0 +1,10 @@
+yash = int(  input("senin nece yasin var")  )
+
+if yash == 100:
+    print("senin bir esidrir yasiyirsan besdi daha")
+
+elif yash == 80:
+    print("vaxtin var hele birazda yasa")
+
+else:
+    print("100 den artiq yasiya bilmezsen")
