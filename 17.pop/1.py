eddf15cee7047943b79f5d6955e6f0f108d4d99c
sahibcyber <isgenderovhaci12@gmail.com>
2026-01-27 21:20:41 +0400

    17.pop

---
+++ 17.pop/1.py
@@ -0,0 +1,15 @@
+# pop() - bu metoddan lsit - in icinden deyer silmek ucun istifade edilir 
+
+# bu metod icindeki string deyerleri yox int deyerleri yazili ryaxud hecne yazilmir 
+
+# eger hecne yazmasaq avtomatik olaraq sonuncu deyeri silir 
+
+
+
+yemek = ["pizza", "doner", "dolma", "aş"]
+
+
+yemek.pop()
+
+for deyerler in yemek:
+    print(deyerler)
