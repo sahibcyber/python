45e992d12090a2a9501abee9a729ab447cddd442
sahibcyber <isgenderovhaci12@gmail.com>
2026-01-27 21:35:52 +0400

    19.sort

---
+++ 19.sort/1.py
@@ -0,0 +1,21 @@
+# sort() metodu deyerleri elifbasi sirasinda gore nizamlaliyir
+
+# sor () metodu movcud siyahini deyisdirir yeni list qaytarmir
+
+# bundan ferqli olaraq sort () funkasiyasi ile hansiki movcud olani yox 
+
+# yeni siayhi yaradir ve movcud siayhi oldugu kimi qalir.
+
+
+telebeler = ["cavidan", "anar", "seymur", "ibrahim", "sahib"]
+
+
+telebeler.sort()
+
+
+for i in telebeler:
+    print(i)
+
+
+
+print(telebeler)
