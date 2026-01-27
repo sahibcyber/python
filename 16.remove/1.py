780c10cbc9f109880a3fb2d5183d5e1d51d1f597
sahibcyber <isgenderovhaci12@gmail.com>
2026-01-27 21:15:15 +0400

    16.remove

---
+++ 16.remove/1.py
@@ -0,0 +1,9 @@
+# remove() - bu metoddan lsit - in icinden deyer silmek ucun istifade edilir 
+
+yemek = ["pizza", "doner", "dolma", "aş"]
+
+yemek.remove("pizza")
+
+
+for i in yemek:
+    print(i)
