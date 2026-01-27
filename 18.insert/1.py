cfc2d4d72b47a0771f4fd5adaf98eb997db20272
sahibcyber <isgenderovhaci12@gmail.com>
2026-01-27 21:27:14 +0400

    18.insert

---
+++ 18.insert/1.py
@@ -0,0 +1,16 @@
+# insert() - bu metoddan list - in icine deeyr elave etmek ucun istifade edilir 
+
+# yeni list - in icindeki isterdiyimiz yere isterdiyimiz deyeri elave ede bilerik
+
+
+# 1 ci arqument hara elave etmek isteryirik onu yaziriq 
+
+# 2 ci arqument ne elave etme isteyirikse onu yaziriq 
+
+
+yemek = ["pizza", "doner", "dolma", "aş"]
+
+yemek.insert(2,"maxara")
+
+for deyerler in yemek:
+    print(deyerler)
