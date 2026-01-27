b5af0e9b5520ca7c0f37cf06656cb378611b7721
sahibcyber <isgenderovhaci12@gmail.com>
2026-01-27 20:22:21 +0400

    13.while

---
+++ 13.while/1.py
@@ -0,0 +1,7 @@
+while True:
+    ad=input('bi deyer daxil edin')
+    
+
+
+# bu o demekdi ki sonsuz yazd;rma demekdi bunu yazmaq tehlukelidir 
+# cunku sisteme saniyeler icind sonsuz yazdirmaq demekdir 
---
+++ 13.while/2.py
@@ -0,0 +1,9 @@
+while True:
+    ad=input("bi deyer daxil edin")
+    if ad !="":
+
+        break
+
+
+
+# break o demekdir ki sonlandir bunu yazdiqda hec bir emeliyyat getmir
