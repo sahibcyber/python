999675a337100867f453b9b87b2e207b3e5a5d09
sahibcyber <isgenderovhaci12@gmail.com>
2026-02-02 20:26:46 +0400

    26.find

---
+++ 26.find/1.py
@@ -0,0 +1,7 @@
+# find bu metod elementin necenci indeksde oldugunu bildirir 
+
+# elementleri indeksleri 0 dan basliyir
+
+cumle =  "hello , welcome to my world"
+
+print(   cumle.find(welcome)   )
