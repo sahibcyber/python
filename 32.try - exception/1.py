c4afe14cce1008c0a339ab85156fc7d10dacc21f
sahibcyber <isgenderovhaci12@gmail.com>
2026-02-03 20:53:32 +0400

    try - exception

---
+++ 32.try - exception/1.py
@@ -0,0 +1,26 @@
+# finaly - kod ister xeta versin ister xeta vermesin, yenede en sonda, finalda nese 
+
+# ekrana yazdirmaq isteyirikse yaxud her hansisa bir kod ise salmaq isteyirikse finally kod 
+
+# blokunan istifade edirik
+
+try:
+    bolunen = int(input("bir deyer daxiil edin"))
+    bolen = int(input("bir eded daxil edin"))
+    netice = bolunen / bolen  
+except ZeroDivisionError as e:
+    print(e)
+    print("sifira bolmek olmaz")
+except ValueError as e:
+    print(e)
+    print("string-e bolmek olmaz")
+except Exception as e:
+    print(e)
+    print("bir xeeta var") 
+else:
+    print("netice")
+finally("bura her zaman isleyecek hissedir") 
+
+
+
+    
