e0a9dd1e654c07770a998bca32abdd5cb9c6618a
sahibcyber <isgenderovhaci12@gmail.com>
2026-02-03 23:20:33 +0400

    35.pip

---
+++ 35.pip/1.py
@@ -0,0 +1,29 @@
+# 1) modul nedir ?  modul ' .py olan fayllardir. bu fayllarin icindeki funkisiyalar , klaslar , variable-ler ve.s olur.
+  
+
+
+# 2) paket nedir ? loru dilde desek paket bir qovulluqdur hasninin ki, icinde birden cox ferqli qovluqlaq ve modullar ola biler .
+# bes niye onda qovluq yox paket deyilir ? cunki paket termini istifade edildikde , hemin qovluqun yeni paketin xususi vezifesinin
+# odlugu anlasilir.
+
+
+# 3) kitabxana nedir ? kitabxana ve paket terminleri demek olar ki , eynidir. paketin icinde kitabxanalar olmur anccaqbir kitabxananin 
+# icinde birden cox paket olur biler .
+#! kitabxana Numpy  - reqemler ve murekkeb riyazi dusturlar islemek ucun nezerde tutulmusdur . (Data smslitikde istifade edilir)
+#! kitabxana pillow - sekillerler islemek ucun nezerde tutulmusdur. sekilleri temizlemek, regini deyismek ve.s
+#! kitabxana faker  - proqramlarin funksionlligini test etmek ucundur.
+#! kitabxana django - web sehiflerin yaradilmasinda istifade edilir. (Admin panel)
+
+
+# 4) paketleri iadre etmek ucun istifade edilen sisteme ise pip deyilir .
+#! pip paketleri idare edilmesi ucun istifade edilen idare etme sistemidir. 
+
+
+# terminal pencerede yazilan pip komundalari hansidir:
+    # a) pip                            - bu komanda butun diger pip komandalari elde etmek ucun istifade edilir .
+    # b) pip - version                  - bu komanda pip versiyasini oyrenmek ucundur .
+    # c) pip install --upgrade pip      - bu komanda pip - i yenilemek ucundur.
+    # d) pip list                       - bu komanda pip ile artiq hansi paketleri qurasdirdigimizi oyrenmek ucundur .
+    # e) pip list -- outdated           - bu komanda hansi paketin kohneldiyini oyrenmek ucundur.
+    # f) pip install pygame --upgrade   - bu komanda eger kphnelen paket varsa onu yenilemek ucun istifade olunur.
+    # g)pip install pandas              - bu komanda yeni paket yuklemek ucundur.
