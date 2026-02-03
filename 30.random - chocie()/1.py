645ca5e2d2d99152c4ced2710745e0da7cf66f21
sahibcyber <isgenderovhaci12@gmail.com>
2026-02-03 20:12:04 +0400

    30.random - chocie

---
+++ 30.random - chocie()/1.py
@@ -0,0 +1,9 @@
+# chocie() - bu metod list - in icindeki random deyerleri qaytarmaq ucundur
+
+import random
+
+siyahi= ['qum', 'qaya', 'beton']
+
+randomdeyer = random.choice(siyahi)
+
+print(randomdeyer)
