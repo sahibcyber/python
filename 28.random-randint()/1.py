dc0c4e51c5b2d18cc6d2109cfddbd19bba58ccad
sahibcyber <isgenderovhaci12@gmail.com>
2026-02-03 20:03:48 +0400

    28.random - randint()

---
+++ 28.random-randint()/1.py
@@ -0,0 +1,9 @@
+# random - bu bir moduldur. mmodulu import edirik. yeni xarici cigaririq
+
+# import etdikden sonra random modulana aid olan pyhton kodlari istifade olunur
+
+import random
+
+randomEded =  random.randint(1, 6)
+
+print(randomEded)
